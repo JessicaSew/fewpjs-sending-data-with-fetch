47d6d38cb9aed6938011dac82feb06941f36d135
jessicasewell <sewell.jessica95@gmail.com>
2021-05-16 19:54:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function submitData(name,email) 
     {
-fetch('http://localhost:3000/users', {
+return fetch('http://localhost:3000/users', {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
@@ -23,26 +23,3 @@ fetch('http://localhost:3000/users', {
             document.body.innerHTML= error.message
           }); 
     }
-
-
-// const formData = {
-//     dogName: "Byron",
-//     dogBreed: "Poodle"
-//   };
-  
-//   const configurationObject = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify(formData)
-//   };
-  
-//   fetch("http://localhost:3000/dogs", configurationObject)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(object) {
-//       console.log(object);
-//     }); 
